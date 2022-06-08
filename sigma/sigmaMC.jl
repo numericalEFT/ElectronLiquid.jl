a55f4c33a432192d0729fe2fd2a991beea1b7e4a
Kun Chen <chenkun0228@gmail.com>
2022-06-08 10:07:50 -0400

    add order 4

--- sigma/sigmaMC.jl
+++ sigma/sigmaMC.jl
@@ -24,6 +24,7 @@ println("Build the diagrams into an experssion tree ...")
 partition = [(1, 0, 0),  # order 1
     (2, 0, 0), (1, 1, 0), (1, 0, 1),  #order 2
     (3, 0, 0), (2, 1, 0), (2, 0, 1), (1, 1, 1), (1, 2, 0), (1, 0, 2), #order 3
+    (4, 0, 0), (3, 1, 0), (3, 0, 1), (2, 1, 1), (2, 2, 0), (2, 0, 2), (1, 3, 0), (1, 0, 3), (1, 2, 1), (1, 1, 2) #order 4
 ]
 
 partition = [p for p in sort(partition) if p[1] + p[2] + p[3] <= Order]
