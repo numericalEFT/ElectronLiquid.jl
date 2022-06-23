39dfcfa14f6f6ca7e9da5d48ea2c31a351f4a684
Kun Chen <chenkun0228@gmail.com>
2022-06-23 11:41:55 -0400

    wip

--- common/counterterm.jl
+++ common/counterterm.jl
@@ -13,18 +13,23 @@ const parafileName = joinpath(@__DIR__, "para.csv") # ROOT/common/para.csv
 Merge interaction order and the main order
 (normal_order, G_order, W_order) --> (normal+W_order, G_order)
 """
-function mergeInteraction(data)
-    res = Dict()
-    for (p, val) in data
-        # println(p)
-        mp = (p[1] + p[3], p[2])
-        if haskey(res, mp)
-            res[mp] += val
-        else
-            res[mp] = val
+function mergeInteraction(data::AbstractDict)
+    if all(x -> length(x) == 3, keys(data))
+        res = Dict()
+        for (p, val) in data
+            @assert length(p) == 3
+            # println(p)
+            mp = (p[1] + p[3], p[2])
+            if haskey(res, mp)
+                res[mp] += val
+            else
+                res[mp] = val
+            end
         end
+        return res
+    else # nothing to merge
+        return data
     end
-    return res
 end
 
 """
@@ -63,24 +68,14 @@ function chemicalpotential_renormalization(order, data, δμ)
     end
     if order >= 4
         # Σ4 = Σ40+Σ11*δμ3+Σ12*(2*δμ1*δμ2)+Σ13*δμ1^3+Σ21*δμ2+Σ22*δμ1^2+Σ31*δμ1
-        # z[4] = _z[(4, 0)] + δμ[1] * _z[(3, 1)] + δμ[1]^2 * _z[(2, 2)] + δμ[2] * _z[(2, 1)]+ (δμ[1])^3 * _z[(1, 3)] + 2 * δμ[1] * δμ[2] * _z[(1, 2)] + δμ[3] * _z[(1, 1)]
-        z[4] = d[(4, 0)] + δμ[2] * d[(2, 1)] + δμ[3] * d[(1, 1)]
+        z[4] = d[(4, 0)] + δμ[1] * d[(3, 1)] + δμ[1]^2 * d[(2, 2)] + δμ[2] * d[(2, 1)] + (δμ[1])^3 * d[(1, 3)] + 2 * δμ[1] * δμ[2] * d[(1, 2)] + δμ[3] * d[(1, 1)]
+        # z[4] = d[(4, 0)] + δμ[2] * d[(2, 1)] + δμ[3] * d[(1, 1)]
     end
     return z
 end
 
 """
     function z_renormalization(order, data, δz, nbody::Int)
-
-    By definition, the z-factor renormalization is defined as
-    DR10 = D10
-
-    DR20 = D20 + D10*δz1
-    DR11 = D11 
-
-    DR30 = D30 + D10*δz2 + D10*δz1^2 + D20*δz1
-    DR21 = D21 + D11*δz1 # D12 has two place two insert δz1
-    DR12 = D12 
 
     By defintion, z=1+δz1+δz2+...
 
@@ -94,22 +89,44 @@ end
 
 # Arguments
 order : total order
-data  : Dict{Order_Tuple, Actual_Data}, where Order_Tuple is a tuple of two integer Tuple{Normal_Order+W_Order, G_Order}
+data  : Vector of data, or Dict{Order_Tuple, Actual_Data}, where Order_Tuple is a tuple of two integer Tuple{Normal_Order+W_Order, G_Order} or three integer Tuple{Normal_Order, G_Order, W_Order}
 δz    : z-factor renormalization for each order
 nbody : nbody=1 for the one-body vertex function (self-energy, or Γ3) and nbody=2 for the two-body vertex function
 """
 function z_renormalization(order, data, δz, nbody::Int)
     @assert order <= 2 "Order $order hasn't been implemented!"
-
-    d = data
-    nd = Dict()
-    if order >= 1
-        nd[(1, 0)] = d[(1, 0)]
+    @assert data isa Dict
+    @assert order <= length(δz) + 1
+    function addOneZ(order, data::AbstractDict, δz)
+        nd = Dict()
+        for orders in keys(data)
+            if sum(orders) > order
+                continue
+            end
+            nd[orders] = deepcopy(data[orders])
+            maxO = orders[1] #the highest order
+            others = orders[2:end]
+            for o in 1:maxO-1
+                lower = (maxO - o, others...)
+                nd[orders] += data[lower] * δz[o]
+            end
+        end
     end
-    if order >= 2
-        nd[(2, 0)] = d[(2, 0)] + d[(1, 0)] * δz[1]
+    function addOneZ(order, data::AbstractVector, δz)
+        @assert order <= length(data)
+        nd = deepcopy(data[1:order])
+        for _order in 1:order
+            maxO = _order #the highest order
+            for o in 1:maxO-1
+                nd[_order] += data[maxO-o] * δz[o]
+            end
+        end
     end
-    return nd
+    #nobody vertex function requires z^n factor
+    for n in 1:nbody
+        data = addOneZ(order, data, δz)
+    end
+    return data
 end
 
 """
@@ -130,6 +147,7 @@ data  : Dict{Order_Tuple, Actual_Data}, where Order_Tuple is a tuple of two inte
 """
 function chemicalpotential(order, Σ, isfock)
     @assert order <= 4 "Order $order hasn't been implemented!"
+    Σ = mergeInteraction(Σ)
 
     δμ = Vector{Any}(undef, order)
     μ = Vector{Any}(undef, order)
@@ -162,6 +180,49 @@ function chemicalpotential(order, Σ, isfock)
         println("order $o:  μ = $(μ[o])  δμ = $(δμ[o])")
     end
     return μ, δμ
+end
+
+"""
+    function chemicalpotential(order, Σ, isfock)
+
+    Derive the chemicalpotential shift and the chemical potential counterterm for each order from the self-energy
+
+    By definition, the chemical potential renormalization is defined as
+    Σ1 = Σ10
+    Σ2 = Σ20+Σ11*δμ1
+    Σ3 = Σ30+Σ11*δμ2+Σ12*δμ1^2+Σ21*δμ1
+    Σ4 = Σ40+Σ11*δμ3+Σ12*(2*δμ1*δμ2)+Σ13*δμ1^3+Σ21*δμ2+Σ22*δμ1^2+Σ31*δμ1
+
+# Arguments
+order : total order
+data  : Dict{Order_Tuple, Actual_Data}, where Order_Tuple is a tuple of two integer Tuple{Normal_Order+W_Order, G_Order}
+δμ    : chemical potential renormalization for each order
+"""
+function derive_onebody_parameter_from_sigma(order, μ, z=zeros(order); isfock=false)
+    δz = zeros(order)
+    δμ = zeros(order)
+    μR = mergeInteraction(μ)
+    zR = mergeInteraction(z)
+    for o in 1:order
+        zR = z_renormalization(o, zR, δz, 1)
+        zR = chemicalpotential_renormalization(o, zR, δμ)
+
+        μR = z_renormalization(o, μR, δz, 1)
+        μR = chemicalpotential_renormalization(o, μR, δμ)
+        if isfock && o == 1
+            δμ[o] = 0.0
+            δz[o] = 0.0
+        else
+            δμ[o] = -μR[o]
+            δz[o] = zR[o]
+        end
+    end
+
+    println("Onebody counterterm:")
+    for o in 1:order
+        println("order $o:  δμ = $(δμ[o]), δz = $(δz[o])")
+    end
+    return δμ, δz
 end
 
 
