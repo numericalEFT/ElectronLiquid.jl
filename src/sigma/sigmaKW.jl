e0d768285377a90d26b7be414e307f67c4f6a1db
Kun Chen <chenkun0228@gmail.com>
2022-09-03 19:00:13 -0400

    vegas not quite correct yet

--- src/sigma/sigmaKW.jl
+++ src/sigma/sigmaKW.jl
@@ -1,27 +1,39 @@
 
-function integrandKW(config)
-    para, diag, extT, kgrid, ngrid = config.para
-    diagram = diag[config.curr]
+function integrandKW(idx, varK, varT, N, ExtKidx, config)
+    para, diag, extT, kgrid, ngrid = config.userdata
+    diagram = diag[idx]
     weight = diagram.node.current
-    l = config.var[3][1]
-    k = config.var[4][1]
-    varK, varT = config.var[1], config.var[2]
+    l = N[1]
+    k = ExtKidx[1]
     varK.data[1, 1] = kgrid[k]
     wn = ngrid[l]
 
     ExprTree.evalKT!(diagram, varK.data, varT.data, para)
-    w = sum(weight[r] * phase(varT, extT[config.curr][ri], wn, para.β) for (ri, r) in enumerate(diagram.root))
+    w = sum(weight[r] * phase(varT, extT[idx][ri], wn, para.β) for (ri, r) in enumerate(diagram.root))
 
-    loopNum = config.dof[config.curr][1]
+    loopNum = config.dof[idx][1]
     factor = 1.0 / (2π)^(para.dim * loopNum)
     return w * factor #the current implementation of sigma has an additional minus sign compared to the standard defintion
 end
 
-function measureKW(config)
-    l = config.var[3][1]
-    k = config.var[4][1]
-    o = config.curr
-    config.observable[o, l, k] += config.relativeWeight
+#for vegas algorithm
+function integrandKW(varK, varT, N, ExtKidx, config)
+    return integrandKW(1, varK, varT, N, ExtKidx, config)
+end
+
+function measureKW(idx, obs, weight, config)
+    l = config.var[3][1]  #matsubara frequency
+    k = config.var[4][1]  #K
+    obs[idx][l, k] += weight
+end
+
+#for vegas algorithm
+function measureKW(obs, weights, config)
+    l = config.var[3][1]  #matsubara frequency
+    k = config.var[4][1]  #K
+    for o in 1:config.N
+        obs[o][l, k] += weights[o]
+    end
 end
 
 function KW(para::ParaMC, diagram;
@@ -42,46 +54,50 @@ function KW(para::ParaMC, diagram;
     K.data[:, 1] .= 0.0
     K.data[1, 1] = kF
     # T = MCIntegration.Tau(β, β / 2.0, offset=1)
-    T = MCIntegration.Continuous(0.0, β, offset=1, alpha=alpha)
+    T = MCIntegration.Continuous(0.0, β; grid=collect(LinRange(0.0, β, 1000)), offset=1, alpha=alpha)
     T.data[1] = 0.0
     X = MCIntegration.Discrete(1, length(ngrid), alpha=alpha)
     ExtKidx = MCIntegration.Discrete(1, length(kgrid), alpha=alpha)
 
     dof = [[p.innerLoopNum, p.totalTauNum - 1, 1, 1] for p in diagpara] # K, T, ExtKidx
-    obs = zeros(ComplexF64, length(dof), length(ngrid), length(kgrid)) # observable for the Fock diagram 
+    # observable of sigma diagram of different permutations
+    obs = [zeros(ComplexF64, length(ngrid), length(kgrid)) for o in 1:length(dof)]
 
     # if isnothing(neighbor)
     #     neighbor = UEG.neighbor(partition)
     # end
     if isnothing(config)
-        config = MCIntegration.Configuration(;
+        config = Configuration(;
             var=(K, T, X, ExtKidx),
             dof=dof,
+            type=ComplexF64, # type of the integrand
             obs=obs,
-            para=(para, diag, extT, kgrid, ngrid),
+            userdata=(para, diag, extT, kgrid, ngrid),
             kwargs...
             # neighbor=neighbor,
             # reweight_goal=reweight_goal, kwargs...
         )
     end
 
-    result = MCIntegration.sample(config, integrandKW, measureKW; print=print, neval=neval, kwargs...)
+    result = integrate(integrandKW; config=config, measure=measureKW, print=print, neval=neval, kwargs...)
     # niter=niter, print=print, block=block, kwargs...)
 
     if isnothing(result) == false
 
-        avg, std = result.mean, result.stdev
         if print >= 0
-            MCIntegration.summary(result.config)
-            println(MCIntegration.summary(result, [o -> real(o[i]) for i in 1:length(dof)]))
+            report(result.config)
+            println(report(result, o -> real(first(o))))
         end
 
-        r = measurement.(real(avg), real(std))
-        i = measurement.(imag(avg), imag(std))
-        data = Complex.(r, i)
-        datadict = Dict{eltype(partition),typeof(data[1, :, :])}()
-        for i in 1:length(dof)
-            datadict[partition[i]] = data[i, :, :]
+        # datadict = Dict{eltype(partition),Complex{Measurement{Float64}}}()
+        datadict = Dict{eltype(partition),Any}()
+
+        for o in 1:length(dof)
+            avg, std = result.mean[o], result.stdev[o]
+            r = measurement.(real(avg), real(std))
+            i = measurement.(imag(avg), imag(std))
+            data = Complex.(r, i)
+            datadict[partition[o]] = data
         end
         return datadict, result
     else
