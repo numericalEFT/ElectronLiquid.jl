fa6d631f051fa75c7d488d44a4dfd38160351d9a
Kun Chen <chenkun0228@gmail.com>
2023-02-12 09:21:57 -0500

    improve zfactor and plot_z

--- example/sigma/zfactor.jl
+++ example/sigma/zfactor.jl
@@ -7,19 +7,30 @@ using ElectronLiquid
 using Printf
 using PyCall
 using LinearAlgebra
+using Plots
 
 curve_fit = pyimport("scipy.optimize").curve_fit
 
-rs = [4.0, ]
-mass2 = [0.01, 0.003,  0.001, 0.0003, 0.0001]
+rs = [2.0, 4.0, 6.0]
+mass2 = [0.01, 0.003, 0.001, 0.0003, 0.0001]
 Fs = [-0.0,]
-beta = [25.0, ]
+beta = [25.0,]
 order = [3,]
 
-const filename = "para.csv"
-# const filename = "para_wn_1minus0.csv"
+rs_old = [1.0, 2.0, 3.0, 4.0]
+z_old = [0.8725, 0.7984, 0.7219, 0.6571]
+z_error_old = [0.0002, 0.0002, 0.0002, 0.0002]
 
-cache =[]
+nz = zeros(order[1], length(rs))
+nz_error = similar(nz)
+z = similar(nz)
+z_error = similar(nz)
+
+
+# const filename = "para.csv"
+const filename = "para_wn_1minus0.csv"
+
+cache = []
 
 # @. model(x, p) = p[1] * x + p[2]
 
@@ -69,23 +80,29 @@ for (_rs, _F, _beta, _order) in Iterators.product(rs, Fs, beta, order)
             @printf("%8d   %24s   %24s     %24s\n", o, "$(dmu[o])", "$(z1[o])", "$(z2[o])")
         end
     end
+
+    rsi = searchsortedfirst(rs, _rs)
     println("without renormalization fit")
     for o in 1:_order
-        x = mass2.^0.5
+        x = mass2 .^ 0.5
         popt, pcov = curve_fit(py"f", x, norenorm_z[o, :, 1])
         println(o, " ", popt[1], " +- ", sqrt.(diag(pcov))[1], " ", popt[2], " +- ", sqrt.(diag(pcov))[2])
+        nz[o, rsi] = popt[2]
+        nz_error[o, rsi] = sqrt.(diag(pcov))[2]
     end
     println("with renormalization fit")
     for o in 1:_order
-        x = mass2.^0.5
+        x = mass2 .^ 0.5
         popt, pcov = curve_fit(py"f", x, renorm_z[o, :, 1])
         println(o, " ", popt[1], " +- ", sqrt.(diag(pcov))[1], " ", popt[2], " +- ", sqrt.(diag(pcov))[2])
+        z[o, rsi] = popt[2]
+        z_error[o, rsi] = sqrt.(diag(pcov))[2]
     end
 end
 
-b1=(cache[1]-cache[2])/(sqrt(0.01)-sqrt(0.001))
-b2=(cache[2]-cache[3])/(sqrt(0.001)-sqrt(0.0001))
-println(b1,", ", b2)
-c1, c2 = cache[2]-b1*sqrt(0.001), cache[3]-b2*sqrt(0.0001)
-println(c1,", ", c2)
-println(1.0/(1+c1),", ", 1/(1+c2))
+p = plot(rs, nz[3, :], yerror=abs.(nz[3, :] .- nz[2, :]) .+ nz_error[3, :], label="without renormalization")
+plot!(p, rs, z[3, :], yerror=abs.(z[3, :] .- z[2, :]) .+ z_error[3, :], label="with renormalization")
+plot!(p, rs_old, z_old, yerror=z_error_old, label="Previous")
+display(p)
+readline()
+
