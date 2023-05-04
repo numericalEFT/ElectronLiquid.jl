014ff8c0f7d45c7af0bcfe4a7723c2d0b84928dd
Kun Chen <chenkun0228@gmail.com>
2023-05-03 23:14:27 -0400

    benchmark vertex3 counterterm in the one-loop vertex4

--- example/ver4/derive_l.jl
+++ example/ver4/derive_l.jl
@@ -3,24 +3,25 @@ using Measurements
 using Printf
 using JLD2
 using PrettyTables
+using ElectronGas
 
 const filename = "ver4_L.jld2"
 
-rs = [5.0,]
-mass2 = [0.01, ]
-Fs = [-0.0, ]
-beta = [25.0, ]
+rs = [1.0,]
+mass2 = [0.01,]
+Fs = [-0.0, -0.2]
+beta = [25.0,]
 order = [2,]
 
-function addbare!(datatuple)
-    para, kgrid, lgrid, ver4 = datatuple
+function addbare!(para, datatuple)
+    kgrid, lgrid, ver4 = datatuple
     data100 = zeros(Complex{Measurement{Float64}}, 2, length(lgrid), length(kgrid))
     for (li, l) in enumerate(lgrid)
         for (ki, k) in enumerate(kgrid)
-            Ws, Wa = Ver4.projected_exchange_interaction(0, para, Ver4.exchange_interaction; kamp =k, verbose=0)
+            Ws, Wa = Ver4.projected_exchange_interaction(0, para, Ver4.exchange_interaction; kamp=k, verbose=0)
             Wuu, Wud = Ver4.sa2ud(Ws, Wa)
-            data100[1, li, ki] = Ws
-            data100[2, li, ki] = Wa
+            data100[1, li, ki] = Wuu
+            data100[2, li, ki] = Wud
         end
     end
     ver4[(1, 0, 0)] = data100
@@ -30,33 +31,41 @@ end
 
 # end
 
-function process200(datatuple)
+function process200(para, datatuple)
     # z = CounterTerm.chemicalpotential_renormalization(para.order, sw, dmu)
     key = (2, 0, 0)
-    li=1
-    para, kgrid, lgrid, ver4 = datatuple
-    addbare!(datatuple)
+    li = 1
+    kgrid, lgrid, ver4 = datatuple
+    addbare!(para, datatuple)
 
-    df = CounterTerm.fromFile()
-    mu, sw = CounterTerm.getSigma(df, UEG.paraid(para), para.order)
-    dzi, dmu, dz = CounterTerm.sigmaCT(para.order, mu, sw)
+    # df = CounterTerm.fromFile()
+    # mu, sw = CounterTerm.getSigma(df, UEG.paraid(para), para.order)
+    # dzi, dmu, dz = CounterTerm.sigmaCT(para.order, mu, sw)
 
+    sigdyn, sigint = SelfEnergy.G0W0(para.basic, int_type=:ko_const, Fs=-para.Fs)
+    z = SelfEnergy.zfactor(para.basic, sigdyn, ngrid=[-1, 0])[1]
+    dz = [1 - 1 / z,]
     # println(sw)
     # println(dz)
     # dz =[-0.509, ]
+    # println(ver4[(1, 0, 0)][1, 1, 1], ", ", ver4[(1, 0, 0)][2, 1, 1])
+    # println((ver4[(1, 0, 0)][1, 1, 1] + ver4[(1, 0, 0)][2, 1, 1]) / 2)
+    # println("before: ", (ver4[(2, 0, 0)][1, 1, 1] + ver4[(2, 0, 0)][2, 1, 1]) / 2)
+    # println(dz)
     ver4 = CounterTerm.z_renormalization(2, ver4, dz, 2)
+    # println("after: ", (ver4[(2, 0)][1, 1, 1] + ver4[(2, 0)][2, 1, 1]) / 2)
     # println(ver4)
 
-    kF=para.kF
+    kF = para.kF
     printstyled("l=$(lgrid[li]) for $(UEG.short(para))\n", color=:green)
     for (p, data) in ver4
         # head = ["k/kF", "uu", "ud", "symmetric", "asymmetric"]
 
         printstyled(@sprintf("%12s    %24s    %24s    %24s    %24s     %24s\n",
-        "k/kF", "uu", "ud", "symmetric", "asymmetric", "p: $p"), color=:yellow)
+                "k/kF", "uu", "ud", "symmetric", "asymmetric", "p: $p"), color=:yellow)
         for (ki, k) in enumerate(kgrid)
             factor = 1.0
-            d1, d2 = real(data[1, li, ki])*factor, real(data[2, li, ki])*factor
+            d1, d2 = real(data[1, li, ki]) * factor, real(data[2, li, ki]) * factor
             # s, a = (d1 + d2) / 2.0, (d1 - d2) / 2.0
             s, a = Ver4.ud2sa(d1, d2)
             @printf("%12.6f    %24s    %24s    %24s    %24s\n", k / kF, "$d1", "$d2", "$s", "$a")
@@ -65,7 +74,7 @@ function process200(datatuple)
 
     printstyled("summed\n", color=:red)
     printstyled(@sprintf("%12s    %24s    %24s    %24s    %24s\n",
-    "k/kF", "uu", "ud", "symmetric", "asymmetric"), color=:yellow)
+            "k/kF", "uu", "ud", "symmetric", "asymmetric"), color=:yellow)
     for (ki, k) in enumerate(kgrid)
         data = ver4[(1, 0)] .+ ver4[(2, 0)]
         d1, d2 = real(data[1, li, ki]), real(data[2, li, ki])
@@ -92,8 +101,8 @@ if abspath(PROGRAM_FILE) == @__FILE__
         para = UEG.ParaMC(rs=_rs, beta=_beta, Fs=_F, order=_order, mass2=_mass2, isDynamic=true)
         kF = para.kF
         for key in keys(f)
-            if UEG.paraid(f[key][1]) == UEG.paraid(para)
-                process200(f[key])
+            if key == UEG.short(para)
+                process200(para, f[key])
             end
         end
     end
