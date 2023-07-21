3de9139d6d8e810fe8334161f76a8aff292616ad
Kun Chen <chenkun0228@gmail.com>
2023-07-20 20:06:50 -0400

    bug fixed

--- test/vertex4PP.jl
+++ test/vertex4PP.jl
@@ -39,9 +39,9 @@ end
 
 @testset "PP" begin
     p = (1, 0, 0)
-    rs = 5.0
+    rs = 1.0
     beta = 25
-    mass2 = 1e-6
+    mass2 = 0.1
     neval = 4e6
     para = ElectronLiquid.ParaMC(rs=rs, beta=beta, Fs=0.0, order=1, mass2=mass2, isDynamic=true)
     UEG.MCinitialize!(para)
@@ -49,9 +49,9 @@ end
     diagram = Ver4.diagram(para, [p,]; channel=[], filter=[])
 
     ############################ generic PH one-angle average ###########################
-    nlist = [0, 1, 2]
+    nlist = [0, 5, 10]
     paras = [Ver4.OneAngleAveraged(para, [para.kF, para.kF], [[0, nlist[1], -1], [0, nlist[2], -1], [0, nlist[3], -1]], :PP, 0),]
-    data, result = Ver4.one_angle_averaged(paras, diagram; neval=neval, print=-1)
+    data, result = Ver4.one_angle_averaged(paras, diagram; neval=neval, print=0)
     obs = data[p]
     println("obs 1:", obs[:, 1, 1])
     println("obs 2:", obs[:, 2, 1])
