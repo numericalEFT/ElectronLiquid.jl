7fb7ef3178854b054ea1f9e04a5d610124881eca
Xiansheng Cai <iintsjds@gmail.com>
2023-09-14 11:33:24 -0400

    wip

--- example/test_PPdiverge.jl
+++ example/test_PPdiverge.jl
@@ -1,10 +1,13 @@
 using ElectronLiquid
 
-para = ParaMC(rs=5.0, beta=25.0, mass2=0.001,
+neval = 1e7
+# channel = [PPr, PHEr, PHr]
+channel = [PPr,]
+para = ParaMC(rs=5.0, beta=25.0, mass2=0.000001,
     Fs=-0.0, Fa=-0.0, isDynamic=true, order=2)
 p = Ver4.OneAngleAveraged(para,
     [para.kF, para.kF], [[-1, 0, 0, -1],], :PP, 0)
 diag = Ver4.diagram(para, UEG.partition(2);
-    channel=[PPr, PHEr, PHr])
-data, res = ElectronLiquid.Ver4.one_angle_averaged([p,], diag)
+    channel=channel)
+data, res = ElectronLiquid.Ver4.one_angle_averaged([p,], diag; neval=neval)
 println(data[(2, 0, 0)])
