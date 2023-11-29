c57015914ad1a9cbe2d830649185c8b777796d27
houpc <houpc96@gmail.com>
2023-11-30 01:13:20 +0800

    add native compileC option

--- src/sigma/sigmaGV_compile.jl
+++ src/sigma/sigmaGV_compile.jl
@@ -15,13 +15,13 @@ function GVcompileJulia_toFile(maxOrder::Int, FeynGraphs, labelProd::LabelProduc
 end
 
 function GVcompileC_toFile(partition, FeynGraphs, labelProd::LabelProduct, 
-    root_dir=joinpath(@__DIR__, "source_codeGV"); datatype::DataType=Float64)
+    root_dir=joinpath(@__DIR__, "source_codeGV"); c_source="func_sigmaGV.c", datatype::DataType=Float64)
 
     ### compile and save the generated Cfunction to a source code file.
     leaf_maps = Vector{Dict{Int,FeynmanGraph}}()
     for key in partition
         key_str = join(string.(key))
-        leafmap = Compilers.compile_C(FeynGraphs[key][1], joinpath(root_dir, "func_sigmaGV.c"); func_name="eval_graph$(key_str)", datatype=datatype)
+        leafmap = Compilers.compile_C(FeynGraphs[key][1], joinpath(root_dir, c_source); func_name="eval_graph$(key_str)", datatype=datatype)
         push!(leaf_maps, leafmap)
     end
 
@@ -46,10 +46,15 @@ function loopbasis_toFile(maxOrder::Int, labelProd::LabelProduct, root_dir=joinp
 end
 
 function GVcompileC_so(partition, datatype::DataType=Float64; c_source=joinpath(@__DIR__, "source_codeGV", "func_sigmaGV.c"), 
-    lib_path=joinpath(@__DIR__, "source_codeGV"), lib_name="sigmaGV", compiler::String="gcc")
-    ### compile C source file to *.so library
+    lib_path=joinpath(@__DIR__, "source_codeGV"), lib_name="sigmaGV", compiler::String="gcc", isnative::Bool=false)
     lib = joinpath(lib_path, "$lib_name.so")
-    run(`$compiler -shared -fPIC -o $lib $c_source -O3`)
+    ### compile C source file to *.so library
+    if isnative 
+        command = `$compiler -shared -fPIC -o $lib $c_source -O3 -march=native`
+    else
+        command = `$compiler -shared -fPIC -o $lib $c_source -O3`
+    end
+    run(command)
 
     ### generate the C wrapper Julia functions and save them in the same path of C library
     GV_Cwrapper(partition, datatype, lib_path=lib_path, lib_name=lib_name)
