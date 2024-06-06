eb588140c2ede974a1607656a3f07c3482b258be
Lizhiyi <peter0627@mail.ustc.edu.cn>
2024-06-06 10:36:32 +0800

    adjust the sourcecodefile name

---
+++ src/vertex4/source_codeParquetAD_NoAlli/Cwrapper_vertex4_ParquetAD.jl
@@ -0,0 +1,124 @@
+
+function eval_vertex4_ParquetAD000!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph000(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD001!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph001(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD002!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph002(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD003!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph003(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD004!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph004(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD005!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph005(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD100!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph100(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD101!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph101(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD102!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph102(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD103!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph103(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD104!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph104(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD110!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph110(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD111!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph111(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD112!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph112(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD113!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph113(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD120!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph120(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD121!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph121(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD122!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph122(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD130!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph130(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD131!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph131(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD140!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph140(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD200!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph200(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD201!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph201(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD202!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph202(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD203!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph203(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD210!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph210(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD211!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph211(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD212!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph212(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD220!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph220(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD221!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph221(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD230!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph230(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD300!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph300(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD301!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph301(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD302!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph302(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD310!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph310(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD311!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph311(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD320!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph320(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD400!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph400(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD401!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph401(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD410!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph410(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_vertex4_ParquetAD500!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "vertex4_ParquetAD.so").eval_graph500(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
