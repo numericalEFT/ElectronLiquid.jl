bc960f294a57a26b95d33a484cb45af226983fb2
Xiansheng Cai <iintsjds@gmail.com>
2024-07-08 19:21:01 +0800

    wip

--- test/vertex3.jl
+++ test/vertex3.jl
@@ -1,3 +1,6 @@
+
+import FeynmanDiagram.FrontEnds: Filter, NoHartree, NoFock, DirectOnly, Wirreducible, Girreducible, NoBubble, Proper
+
 function compare(data, expect)
     # println(data, ", ", expect)
     @test isapprox(data.val, expect, atol=5 * data.err)
@@ -11,15 +14,39 @@ end
     #     println(ver3[(2, 0, 0)])
     # end
 
-    @testset "Vertex3 Dynamic O(1)" begin
-        para = UEG.ParaMC(rs=5.0, beta=25.0, Fs=0.0, order=1, mass2=0.01, isDynamic=true)
+    # @testset "Vertex3 Dynamic O(1)" begin
+    #     para = UEG.ParaMC(rs=5.0, beta=25.0, Fs=0.0, order=1, mass2=0.01, isDynamic=true)
+    #     kin = [para.kF,]
+    #     # Nth = 4
+    #     # theta = [(i) / (Nth * π) for i in 0:Nth] # N+1 points
+    #     theta = [0.0,]
+    #     qout = [[para.kF * (1 - cos(θ)), -para.kF * sin(θ), 0.0] for θ in theta]
+    #     ver3, result = Ver3.MC_KW_angle(para; kin=kin, qout=qout, nkin=[0,], nqout=[1,])
+    #     obs = ver3[(1, 0, 0)][:, 1, 1, :, 1]
+    #     println(obs)
+    #     # println(ver3[(2, 0, 0)][:, 1, 1, :, 1])
+
+    #     # for the one-loop vertex3 diagram, we expect
+    #     # \gamma_3(q=0, w->0)  = - dIm\Sigma)/dw_n
+    #     expect = -0.44275
+    #     compare(real(obs[1]), expect)
+
+    # end
+
+    @testset "Vertex3 static" begin
+        para = UEG.ParaMC(rs=1.0, beta=25.0, Fs=0.0, order=3, mass2=3.5, isDynamic=false)
         kin = [para.kF,]
-        # Nth = 4
-        # theta = [(i) / (Nth * π) for i in 0:Nth] # N+1 points
-        theta = [0.0,]
+        Nth = 4
+        theta = [(i) / (Nth * π) for i in 0:Nth] # N+1 points
+        # theta = [0.0,]
         qout = [[para.kF * (1 - cos(θ)), -para.kF * sin(θ), 0.0] for θ in theta]
-        ver3, result = Ver3.MC_KW_angle(para; kin=kin, qout=qout, nkin=[0,], nqout=[1,])
-        println(ver3[(1, 0, 0)][:, 1, 1, :, 1])
+        ver3, result = Ver3.MC_KW_angle(para;
+            kin=kin, qout=qout, nkin=[0,], nqout=[0,])
+        println(ver3)
+        # obs = ver3[(1, 0, 0)][:, 1, 1, :, 1]
+        # println(obs)
+        println(ver3[(1, 0, 0)][:, 1, 1, :, 1] + ver3[(2, 0, 0)][:, 1, 1, :, 1] + ver3[(3, 0, 0)][:, 1, 1, :, 1])
+
     end
 
 end
