b98226341a54daf1d6393df2fb921012fa32b03f
houpc <houpc96@gmail.com>
2026-01-16 00:18:55 +0800

    update code_ParquetAD for sigma

--- src/sigma/source_codeParquetAD/Cwrapper_sigma_ParquetAD.jl
+++ src/sigma/source_codeParquetAD/Cwrapper_sigma_ParquetAD.jl
@@ -17,6 +17,9 @@ end
 function eval_sigma_ParquetAD105!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph105(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD106!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph106(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD110!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph110(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -32,6 +35,9 @@ end
 function eval_sigma_ParquetAD114!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph114(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD115!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph115(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD120!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph120(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -44,6 +50,9 @@ end
 function eval_sigma_ParquetAD123!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph123(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD124!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph124(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD130!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph130(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -53,14 +62,26 @@ end
 function eval_sigma_ParquetAD132!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph132(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD133!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph133(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD140!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph140(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD141!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph141(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD142!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph142(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD150!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph150(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD151!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph151(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD160!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph160(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD200!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph200(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -77,6 +98,9 @@ end
 function eval_sigma_ParquetAD204!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph204(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD205!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph205(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD210!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph210(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -89,6 +113,9 @@ end
 function eval_sigma_ParquetAD213!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph213(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD214!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph214(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD220!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph220(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -98,14 +125,26 @@ end
 function eval_sigma_ParquetAD222!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph222(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD223!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph223(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD230!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph230(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD231!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph231(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD232!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph232(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD240!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph240(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD241!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph241(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD250!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph250(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD300!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph300(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -119,6 +158,9 @@ end
 function eval_sigma_ParquetAD303!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph303(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD304!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph304(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD310!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph310(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -128,14 +170,26 @@ end
 function eval_sigma_ParquetAD312!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph312(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD313!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph313(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD320!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph320(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD321!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph321(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD322!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph322(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD330!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph330(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD331!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph331(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD340!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph340(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD400!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph400(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -146,14 +200,26 @@ end
 function eval_sigma_ParquetAD402!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph402(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD403!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph403(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD410!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph410(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD411!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph411(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD412!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph412(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD420!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph420(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD421!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph421(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD430!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph430(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigma_ParquetAD500!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph500(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -161,9 +227,27 @@ end
 function eval_sigma_ParquetAD501!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph501(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD502!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph502(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD510!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph510(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigma_ParquetAD511!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph511(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD520!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph520(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigma_ParquetAD600!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph600(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD601!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph601(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD610!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph610(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigma_ParquetAD700!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigma_ParquetAD.so").eval_graph700(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
--- src/sigma/source_codeParquetAD/Cwrapper_sigmadk_ParquetAD.jl
+++ src/sigma/source_codeParquetAD/Cwrapper_sigmadk_ParquetAD.jl
@@ -17,6 +17,9 @@ end
 function eval_sigmadk_ParquetAD1051!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1051(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD1061!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1061(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD1101!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1101(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -32,6 +35,9 @@ end
 function eval_sigmadk_ParquetAD1141!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1141(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD1151!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1151(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD1201!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1201(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -44,6 +50,9 @@ end
 function eval_sigmadk_ParquetAD1231!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1231(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD1241!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1241(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD1301!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1301(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -53,14 +62,26 @@ end
 function eval_sigmadk_ParquetAD1321!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1321(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD1331!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1331(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD1401!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1401(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD1411!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1411(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD1421!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1421(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD1501!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1501(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD1511!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1511(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD1601!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph1601(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD2001!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2001(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -77,6 +98,9 @@ end
 function eval_sigmadk_ParquetAD2041!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2041(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD2051!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2051(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD2101!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2101(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -89,6 +113,9 @@ end
 function eval_sigmadk_ParquetAD2131!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2131(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD2141!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2141(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD2201!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2201(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -98,14 +125,26 @@ end
 function eval_sigmadk_ParquetAD2221!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2221(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD2231!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2231(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD2301!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2301(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD2311!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2311(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD2321!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2321(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD2401!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2401(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD2411!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2411(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD2501!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph2501(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD3001!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3001(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -119,6 +158,9 @@ end
 function eval_sigmadk_ParquetAD3031!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3031(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD3041!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3041(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD3101!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3101(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
@@ -128,14 +170,26 @@ end
 function eval_sigmadk_ParquetAD3121!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3121(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD3131!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3131(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD3201!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3201(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD3211!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3211(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD3221!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3221(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD3301!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3301(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD3311!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3311(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD3401!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph3401(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD4001!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4001(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -146,14 +200,26 @@ end
 function eval_sigmadk_ParquetAD4021!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4021(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD4031!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4031(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD4101!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4101(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD4111!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4111(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD4121!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4121(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD4201!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4201(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD4211!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4211(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD4301!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph4301(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
 function eval_sigmadk_ParquetAD5001!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph5001(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
@@ -161,9 +227,27 @@ end
 function eval_sigmadk_ParquetAD5011!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph5011(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD5021!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph5021(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD5101!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph5101(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
+function eval_sigmadk_ParquetAD5111!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph5111(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD5201!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph5201(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
 function eval_sigmadk_ParquetAD6001!(root::Vector{Float64}, leafVal::Vector{Float64})
     @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph6001(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD6011!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph6011(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD6101!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph6101(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
+end
+function eval_sigmadk_ParquetAD7001!(root::Vector{Float64}, leafVal::Vector{Float64})
+    @ccall joinpath(@__DIR__, "sigmadk_ParquetAD.so").eval_graph7001(root::Ptr{Cdouble}, leafVal::Ptr{Cdouble})::Cvoid
 end
